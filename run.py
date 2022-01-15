86fed6c4cab9999730445f7ea7f5bad53d925977
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-15 09:01:18 +0100

    simplify entry point script

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
 from app import create_app
+from app.config import ConfigType
 
-
-app = create_app()
-
-
-def main():
-    app.run(debug=True)
-
+app = create_app(config_type=ConfigType.DEVELOPMENT)
 
 if __name__ == "__main__":
-    main()
+    app.run()
