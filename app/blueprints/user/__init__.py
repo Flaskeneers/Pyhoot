0b936402f13080ef596e7bdc60744fbc23a8e8bf
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-18 10:45:02 +0100

    add User blueprint

---
+++ app/blueprints/user/__init__.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+bp_user = Blueprint(name="user",
+                    import_name=__name__,
+                    template_folder="templates",
+                    static_folder="static")
+
+from . import views
