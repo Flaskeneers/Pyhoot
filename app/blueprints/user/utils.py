ad105ba44e7c272d809ea3792c8ac84a56ef09e6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-18 13:44:17 +0100

    extract out  get_clean_question_form_data() to utils module

---
+++ app/blueprints/user/utils.py
@@ -0,0 +1,7 @@
+def get_clean_question_form_data(form_data: dict) -> dict:
+    return dict(
+        description=form_data["description"],
+        correct_answer=form_data["correct_answer"]["text"],
+        wrong_answers=[wrong_answer["text"] for wrong_answer
+                       in form_data["wrong_answers"]]
+    )
