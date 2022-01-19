dfc2972cfaaa58b5fa19d8a8254e474460fa5fe5
Fredrik <edstrom92@hotmail.com>
2022-01-19 16:40:56 +0100

    Integrated Flask_login

--- app/persistence/models/user.py
+++ app/persistence/models/user.py
@@ -7,3 +7,15 @@ from ..db import db
 class User(Document):
     collection: Collection = db.users
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
