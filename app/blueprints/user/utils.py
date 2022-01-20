dbd1b5e79d7459691f824d1e8a0a8df04e35d169
Fredstrom <91597142+Fredstrom@users.noreply.github.com>
2022-01-20 14:57:03 +0100

    Revert "wip-0003 connect user quiz"

--- app/blueprints/user/utils.py
+++ app/blueprints/user/utils.py
@@ -5,10 +5,3 @@ def get_clean_question_form_data(form_data: dict) -> dict:
         wrong_answers=[wrong_answer["text"] for wrong_answer
                        in form_data["wrong_answers"]]
     )
-
-
-def is_request_args_valid(*args) -> bool:
-    for arg in args:
-        if not arg:
-            return False
-    return True
--- app/persistence/repository/question_repo.py
+++ app/persistence/repository/question_repo.py
@@ -4,18 +4,23 @@ from app.persistence.models.question import Question
 from app.shared.resultlist import ResultList
 
 
-def create(**kwargs) -> Question:
-    question = Question(kwargs)
+def create(description: str, correct_answer: str, wrong_answers: list[str]) -> Question:
+    data = dict(
+        description=description,
+        correct_answer=correct_answer,
+        wrong_answers=wrong_answers
+    )
+    question = Question(data)
     question.save()
     return question
 
 
 def get_all() -> list[Question]:
-    return ResultList(Question(item) for item in Question.find())
+    return ResultList(Question(item) for item in Question.collection.find())
 
 
-def get_by_id(_id: str) -> Question | None:
-    return Question(Question.collection.find_one(dict(_id=ObjectId(_id))))
+def get_by_id(_id: str) -> Question:
+    return ResultList(Question(item) for item in Question.collection.find(dict(_id=ObjectId(_id)))).first_or_none()
 
 
 def update_by_id(_id: str, new_data: dict) -> None:
@@ -25,8 +30,7 @@ def update_by_id(_id: str, new_data: dict) -> None:
 
 def delete_by_id(_id: str) -> None:
     question = get_by_id(_id)
-    if question is not None:
-        question.delete()
+    question.delete()
 
 
 def delete_all(query: dict | None = None) -> int:
