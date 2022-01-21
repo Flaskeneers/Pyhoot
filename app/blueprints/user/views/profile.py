b9853ce97fb5d7e08668eb084edadfa7342bb789
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-21 04:11:01 +0100

    modify to use new decoupled quizzes

--- app/blueprints/user/views/profile.py
+++ app/blueprints/user/views/profile.py
@@ -3,13 +3,15 @@ from flask_login import current_user, login_required
 
 from .. import bp_user
 from ..forms.profile import EditProfileForm
+from app.controllers import quiz as quiz_controller
 
 
 @bp_user.get("/profile")
 @login_required
 def view_profile():
+    quizzes = quiz_controller.get_all_quizzes_by_username(current_user.username)
     return render_template("user/profile/view.html",
-                           quizzes=current_user.my_quizzes)
+                           quizzes=quizzes)
 
 # @bp_user.get("/profile")
 # def edit_profile():
