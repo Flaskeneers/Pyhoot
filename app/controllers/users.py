00534639b9ce814d8d30ca306b91c8adf55f2536
Fredrik <edstrom92@hotmail.com>
2022-01-21 11:26:15 +0100

    Created Signup-styles

---
+++ app/controllers/users.py
@@ -0,0 +1,21 @@
+from app.persistence.repository import user_repo as repo
+
+
+def create_user(email: str, username: str, password: str):
+    return repo.create_user(email, username, password)
+
+
+def get_by_username(username: str):
+    return repo.get_by_username(username)
+
+
+def check_existing_users(username: str, email: str):
+    return repo.check_existing_users(username, email)
+
+
+def verify_password(user, password):
+    return repo.verify_password(user.password, password)
+
+
+def get_all_users():
+    return repo.get_all_users()
