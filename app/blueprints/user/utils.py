addf9f56aa8dc3b5dc3c5ef5d400daf6a20ad617
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-21 03:08:09 +0100

    modify utility function to support newly refactored question form

--- app/blueprints/user/utils.py
+++ app/blueprints/user/utils.py
@@ -1,9 +1,8 @@
 def get_clean_question_form_data(form_data: dict) -> dict:
     return dict(
         description=form_data["description"],
-        correct_answer=form_data["correct_answer"]["text"],
-        wrong_answers=[wrong_answer["text"] for wrong_answer
-                       in form_data["wrong_answers"]]
+        correct_answer=form_data["correct_answer"],
+        wrong_answers=[wrong_answer for wrong_answer in form_data["wrong_answers"]]
     )
 
 
