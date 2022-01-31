28d19de83b112c1dfc708836b3b8c0f9a9c06f43
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-31 15:10:16 +0100

    modify test case to accomodate test db fix

--- tests/controllers/test_user.py
+++ tests/controllers/test_user.py
@@ -1,36 +1,24 @@
 import unittest
 
+from app.controllers import user as user_controller
 from tests import BaseTestCase
 
 
 class UserTestCase(BaseTestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        super().setUpClass()
-
-        # additional logic for testing user
-        from app.controllers import user as user_controller
-        cls.user_controller = user_controller
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        super().tearDownClass()
-        # additional logic
-
     def test_create_user(self):
         # create a user
         email = "jane.doe@example.com"
         username = "janedoe"
         password = "super-secret-password"
-        self.user_controller.create_user(email, username, password)
+        user_controller.create_user(email, username, password)
 
         # verify user was created in database
-        user = self.user_controller.get_by_username(username)
+        user = user_controller.get_by_username(username)
         self.assertIsNotNone(user)
 
         self.assertEqual(user.username, username)
 
-        user = self.user_controller.get_by_username("emperor-fredrik")
+        user = user_controller.get_by_username("emperor-fredrik")
         self.assertIsNone(user)
 
         # 1. create user
@@ -41,12 +29,6 @@ class UserTestCase(BaseTestCase):
         #    - check user is not logged in
         # 4. delete account
         #    - assert user no longer exists in db
-
-    def test_db(self):
-        self.assertEqual(self.app.config["MONGO_DB_NAME"], "pyhoot-mongo-db-test")
-
-        with self.app.app_context():
-            self.assertEqual(self.app.config["MONGO_DB_NAME"], "pyhoot-mongo-db-test")
 
 
 if __name__ == "__main__":
