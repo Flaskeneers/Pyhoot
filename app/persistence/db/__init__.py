67798ca90206d3bc382179805db9ce55f3c1ed5f
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-01-31 15:06:54 +0100

    fix bug for setting up test database

--- app/persistence/db/__init__.py
+++ app/persistence/db/__init__.py
@@ -1,8 +1,22 @@
+from os import getenv
+
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.database import Database
 
-client: MongoClient | None = None
-db: Database | None = None
+load_dotenv()
+
+PROTOCOL = getenv("MONGO_DB_PROTOCOL")
+USER = getenv("MONGO_DB_USER")
+PASS = getenv("MONGO_DB_PASS")
+HOST = getenv("MONGO_DB_HOST")
+PORT = getenv("MONGO_DB_PORT")
+DB_NAME = getenv("MONGO_DB_NAME")
+
+DEFAULT_DB_URI = f"{PROTOCOL}://{USER}:{PASS}@{HOST}:{PORT}"
+
+client: MongoClient = MongoClient(DEFAULT_DB_URI, authSource="admin")
+db: Database = client[f"{DB_NAME}-test"]
 
 
 def init_mongodb(uri: str, name: str) -> None:
